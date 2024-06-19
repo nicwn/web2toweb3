4da61b8ebb6d03516312d810b6d4cb501cf35f76
Nicholas Wang <clone.of.snake@gmail.com>
2024-06-19 13:56:57 +0200

    done with week 2

--- Bank.sol
+++ Bank.sol
@@ -8,6 +8,7 @@ import "hardhat/console.sol";
 // import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
+ * Week 2 day 5
  * A smart contract that allows changing a state variable of the contract and tracking the changes
  * It also allows the owner to withdraw the Ether in the contract
  * @author BuidlGuidl
